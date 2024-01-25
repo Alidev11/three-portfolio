8a5e0f7501bf51c71a797066ef9e2bf3d63caf40
Alidev11 <karwawiali@gmail.com>
2024-01-25 17:57:30 +0100

    Full website added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,8 +16,40 @@ const Contact = () => {
 	});
 	const [loading, setLoading] = useState(false);
 
-	const handleChange = (e) => {};
-	const handleSubmit = (e) => {};
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+
+		setForm({ ...form, [name]: value });
+	};
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setLoading(true);
+		emailjs.send(
+			"service_9v2hmqr",
+			"template_0l8sqs9",
+			{
+				from_name: form.name,
+				to_name: "Ali ELKAROUAOUI",
+				from_email: form.email,
+				to_email: "contact@jsmastery.pro",
+				message: form.message,
+			},
+			"5MSkEFA2ePM8kV_Hi"
+		).then(() => {
+			setLoading(false);
+			alert('Thank you. I will get back to you as soon as possible.');
+
+			setForm({
+				name: '',
+				email: '',
+				message: '',
+			})
+		}, (error) => {
+			setLoading(false)
+			console.log(error);
+			alert('Something went wrong.')
+		})
+	};
 
 	return (
 		<div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
