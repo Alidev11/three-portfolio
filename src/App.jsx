bf5767547aee2aac2521597448ca7954e277efb7
Alidev11 <karwawiali@gmail.com>
2024-01-30 01:51:58 +0100

    vercel speed insights added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import { BrowserRouter } from "react-router-dom";
 import {
 	About,
@@ -30,6 +31,7 @@ const App = () => {
 					<StarsCanvas />
 				</div>
 			</div>
+			<SpeedInsights />
 			<Analytics />
 		</BrowserRouter>
 	);
