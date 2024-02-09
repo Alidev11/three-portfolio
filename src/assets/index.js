7d2219e3d4a54fc863fd67b32acf6cbeafdebd98
Alidev11 <karwawiali@gmail.com>
2024-02-09 18:37:29 +0100

    edit work experience

--- src/assets/index.js
+++ src/assets/index.js
@@ -7,72 +7,56 @@ import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
 
-import css from "./tech/css.png";
 import docker from "./tech/docker.png";
 import figma from "./tech/figma.png";
 import git from "./tech/git.png";
-import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
 import flutter from "./tech/flutter.png";
 import ajax from "./tech/ajax.png";
 import axure from "./tech/axure.png";
-import bootstrap from "./tech/bootstrap.png";
 import firebase from "./tech/firebase.png";
-import graphite from "./tech/graphite.png";
-import jquery from "./tech/jquery.png";
 import laravel from "./tech/laravel.png";
 import mysql from "./tech/mysql.png";
 import php from "./tech/php.png";
 import python from "./tech/python.png";
 import pytorch from "./tech/pytorch.png";
 import sklearn from "./tech/sklearn.png";
-import nodejs from "./tech/nodejs.png";
 import tailwind from "./tech/tailwind.png";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import fiverr from "./company/fiverr.png";
+import bcSkills from "./company/bcSkills.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
 
 export {
-  logo,
-  backend,
-  creator,
-  mobile,
-  web,
-  github,
-  menu,
-  close,
-  css,
-  docker,
-  figma,
-  git,
-  html,
-  javascript,
-  sklearn,
-	jquery,
+	logo,
+	backend,
+	creator,
+	mobile,
+	web,
+	github,
+	menu,
+	close,
+	docker,
+	figma,
+	git,
+	javascript,
+	sklearn,
 	php,
 	ajax,
 	mysql,
-  nodejs,
-  axure,
-	bootstrap,
+	axure,
 	firebase,
-	graphite,
 	flutter,
 	pytorch,
-  tailwind,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
-  carrent,
-  jobit,
-  tripguide,
-  laravel,
-  python,
+	tailwind,
+	carrent,
+	jobit,
+	tripguide,
+	laravel,
+	python,
+	fiverr,
+	bcSkills,
 };
